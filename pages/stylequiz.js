6ec53a5035b22f2f8796ebef96580fcfb0777d87
Phuc Doan <phucdoan252000@gmail.com>
2022-03-02 22:43:19 +0700

    Update layout, create StyleQuiz page

--- pages/stylequiz.js
+++ pages/stylequiz.js
@@ -1,37 +1,56 @@
-import Layout from '../components/layout'
-import Topbar from '../components/topbar'
-import Footer from '../components/footer'
-
+/* eslint-disable @next/next/no-img-element */
+import Layout from "../components/layout";
+import Topbar from "../components/topbar";
+import Footer from "../components/footer";
+import styles from "../styles/StyleQuiz.module.css";
+import Carousel from "../components/carousel";
 
 export default function StyleQuiz() {
   return (
-    <section>
-      <h2>Layout Example (Style Quiz)</h2>
-      <p>
-        This example adds a property <code>getLayout</code> to your page,
-        allowing you to return a React component for the layout. This allows you
-        to define the layout on a per-page basis. Since we're returning a
-        function, we can have complex nested layouts if desired.
-      </p>
-      <p>
-        When navigating between pages, we want to persist page state (input
-        values, scroll position, etc.) for a Single-Page Application (SPA)
-        experience.
-      </p>
-      <p>
-        This layout pattern will allow for state persistence because the React
-        component tree is persisted between page transitions. To preserve state,
-        we need to prevent the React component tree from being discarded between
-        page transitions.
-      </p>
-      <h3>Try It Out</h3>
-      <p>
-        To visualize this, try tying in the search input in the{' '}
-        <code>Sidebar</code> and then changing routes. You'll notice the input
-        state is persisted.
-      </p>
-    </section>
-  )
+    <div className="styleQuizWrapper">
+      <section
+        className="d-lg-flex py-0 full-half-width-block three-up-content alignright"
+        id="block_60022a56a11a8"
+      >
+        <div className="row align-items-center">
+          <div className={"col-12 col-sm-6 text-center text-center color"}>
+            <div className={`row ${styles.rowCustome}`}>
+              <div className={`content ${styles.contentCustome}`}>
+                <div className={`${styles.contentWidth}`}>
+                  <div className="d-flex flex-column justify-content-center align-items-start">
+                    <h1 className={`${styles.title}`}>
+                      What Is My Interior Design Style?
+                      <br />
+                    </h1>
+                    <p>
+                      Need help identifying your personal design style? With all
+                      the inspiration out there, uncovering your style can be
+                      overwhelming. Enter: the Modsy interior design style quiz.
+                      Our fast, free style quiz will help you pinpoint your
+                      interior design style, all in a few minutes or less.
+                    </p>
+                    <p>
+                      <a href="https://www.modsy.com/stylequiz/room">
+                        <button
+                          className="primary cms-page-cta"
+                          style={{ marginTop: 0.5 + "em" }}
+                        >
+                          Take The Quiz
+                        </button>
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-6 col-sm-4 text-center text-center aligned-right ${styles.mainGraphic}`}>
+            <img src="/stylequiz2.png" alt="StyleQuiz img" />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 StyleQuiz.getLayout = function getLayout(page) {
@@ -39,7 +58,7 @@ StyleQuiz.getLayout = function getLayout(page) {
     <Layout>
       <Topbar />
       {page}
-      <Footer/>
+      <Footer />
     </Layout>
-  )
-}
+  );
+};
