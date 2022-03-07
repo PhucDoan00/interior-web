cf0b7aec673a87c096bdaa961011c856bfbd4d3b
Phuc Doan <phucdoan252000@gmail.com>
2022-03-08 00:22:39 +0700

    Update Format Setting

--- pages/designIdeas.js
+++ pages/designIdeas.js
@@ -3,34 +3,7 @@ import Topbar from '../components/topbar'
 import Footer from '../components/footer'
 
 export default function DesignIdeas() {
-  return (
-    <section>
-      <h2>Layout Example (Design Ideas)</h2>
-      <p>
-        This example adds a property <code>getLayout</code> to your page,
-        allowing you to return a React component for the layout. This allows you
-        to define the layout on a per-page basis. Since we're returning a
-        function, we can have complex nested layouts if desired.
-      </p>
-      <p>
-        When navigating between pages, we want to persist page state (input
-        values, scroll position, etc.) for a Single-Page Application (SPA)
-        experience.
-      </p>
-      <p>
-        This layout pattern will allow for state persistence because the React
-        component tree is persisted between page transitions. To preserve state,
-        we need to prevent the React component tree from being discarded between
-        page transitions.
-      </p>
-      <h3>Try It Out</h3>
-      <p>
-        To visualize this, try tying in the search input in the{' '}
-        <code>Sidebar</code> and then changing routes. You'll notice the input
-        state is persisted.
-      </p>
-    </section>
-  )
+  return <div className=""></div>
 }
 
 DesignIdeas.getLayout = function getLayout(page) {
@@ -38,7 +11,7 @@ DesignIdeas.getLayout = function getLayout(page) {
     <Layout>
       <Topbar />
       {page}
-      <Footer/>
+      <Footer />
     </Layout>
   )
 }
