b2812c6b2a8ae5cd4d48674b87819380d9685f3b
minhnab <minhnab@users.noreply.github.com>
2022-03-12 18:32:49 +0700

    First commit

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Footer from '../components/footer'
 import Carousel from '../components/carousel'
 import SmallCarousel from '../components/smallCarousel'
 import CardSlider from '../components/cardSlider'
+import Toprecommend from '../components/toprecommend'
+import Designstyle from '../components/designstyle'
+import Loveit from '../components/loveit'
 
 export default function Index() {
   return <h1>This is Home Page</h1>
@@ -14,7 +17,10 @@ Index.getLayout = function getLayout(page) {
     <Layout>
       <Topbar />
       <Carousel />
+      <Toprecommend />  
       <CardSlider />
+      <Designstyle />
+      <Loveit />
       <SmallCarousel />
       <Footer />
     </Layout>
