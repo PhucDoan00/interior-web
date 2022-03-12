f1564925613abb0e4735bc6744a3a01a89b5acd4
Phuc Doan <phucdoan252000@gmail.com>
2022-03-12 21:46:41 +0700

    Design Ideas Living Room

--- pages/designIdeas.js
+++ pages/designIdeas.js
@@ -25,37 +25,40 @@ export default function DesignIdeas() {
         </div>
         <div className={` mb-5 justify-content-evenly ${styles.category}`}>
           <Link href="/rooms/livingroom">
-            <div className="card" style={{ width: 13 + 'rem', height: 13 + 'rem' }}>
+            <div
+              className={`card ${styles.cardHover}`}
+              style={{ width: 13 + 'rem', height: 13 + 'rem' }}
+            >
               <img src="/designIdeas/living.jpg" className="card-img-top" alt="Living Room" />
               <h5 className={`card-title ${styles.cardTitle}`}>Living Room</h5>
             </div>
           </Link>
           <Link href="/rooms/bedroom">
-            <div className="card" style={{ width: 13 + 'rem' }}>
+            <div className={`card ${styles.cardHover}`} style={{ width: 13 + 'rem' }}>
               <img src="/designIdeas/bed.jpg" className="card-img-top" alt="Bed Room" />
               <h5 className={`card-title ${styles.cardTitle}`}>Bed Room</h5>
             </div>
           </Link>
           <Link href="/rooms/diningroom">
-            <div className="card" style={{ width: 13 + 'rem' }}>
+            <div className={`card ${styles.cardHover}`} style={{ width: 13 + 'rem' }}>
               <img src="/designIdeas/dining.jpg" className="card-img-top" alt="Dining Room" />
               <h5 className={`card-title ${styles.cardTitle}`}>Dining Room</h5>
             </div>
           </Link>
           <Link href="/rooms/office">
-            <div className="card" style={{ width: 13 + 'rem' }}>
+            <div className={`card ${styles.cardHover}`} style={{ width: 13 + 'rem' }}>
               <img src="/designIdeas/office.jpg" className="card-img-top" alt="Office" />
               <h5 className={`card-title ${styles.cardTitle}`}>Office</h5>
             </div>
           </Link>
           <Link href="/rooms/kidsroom">
-            <div className="card" style={{ width: 13 + 'rem' }}>
+            <div className={`card ${styles.cardHover}`} style={{ width: 13 + 'rem' }}>
               <img src="/designIdeas/kids.jpg" className="card-img-top" alt="Kid's Room" />
               <h5 className={`card-title ${styles.cardTitle}`}>{`Kid's Room`}</h5>
             </div>
           </Link>
           <Link href="/rooms/nursey">
-            <div className="card" style={{ width: 13 + 'rem' }}>
+            <div className={`card ${styles.cardHover}`} style={{ width: 13 + 'rem' }}>
               <img src="/designIdeas/nursey.jpg" className="card-img-top" alt="Nursey" />
               <h5 className={`card-title ${styles.cardTitle}`}>Nursey</h5>
             </div>
