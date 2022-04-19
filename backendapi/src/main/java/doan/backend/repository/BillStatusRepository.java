df69940ecd4bb5ddb417ad6b97d9389bb0e243c8
The Anh <anhhltde140260@fpt.edu.vn>
2022-04-19 21:36:07 +0700

    Add Filter by Color in All Products + Filter by Room & Sort in each Room

--- backendapi/src/main/java/doan/backend/repository/BillStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package doan.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import doan.backend.entity.BillStatus;
-
-public interface BillStatusRepository extends JpaRepository<BillStatus, Long>{
-
-}
--- backendapi/src/main/java/doan/backend/repository/StyleRepository.java
+++
@@ -1,9 +0,0 @@
-package doan.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import doan.backend.entity.Style;
-
-public interface StyleRepository extends JpaRepository<Style, Long>{
-
-}
