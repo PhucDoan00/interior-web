42aa12785b79e76ab8022552c078d19aa04d9a52
PhucDoan00 <65746696+PhucDoan00@users.noreply.github.com>
2022-04-25 09:05:02 +0700

    remove local storage, fix price when delete

--- components/withAuth.js
+++ components/withAuth.js
@@ -1,29 +1,39 @@
 import { useRouter } from 'next/router'
+import { confirmAlert } from 'react-confirm-alert'
 
-const withAuth = Component => {
+const withAuth = (Component) => {
+  const Auth = (props) => {
+    const router = useRouter()
+    const { isLoggedIn } = {}
+    const pushLogin = () => {
+      router.push('/login')
+    }
+    if (typeof window !== 'undefined') {
+      // Login data added to local storage
+      isLoggedIn = localStorage.getItem('user')
+    }
+    // If user is not logged in, return to login
+    if (!isLoggedIn) {
+      if (typeof window !== 'undefined') {
+        confirmAlert({
+          title: '',
+          message: 'Please login to continue...',
+          buttons: [
+            {
+              label: 'OK',
+              onClick: () => pushLogin(),
+            },
+          ],
+        })
+      }
+      return null
+    }
 
-    const Auth = (props) => {
-        const router = useRouter()
-        const { isLoggedIn } = {};
-        if (typeof window !== 'undefined') {
-            // Login data added to local storage
-            isLoggedIn = localStorage.getItem('user');
-        }
-        // If user is not logged in, return to login
-        if (!isLoggedIn) {
-            if (typeof window !== 'undefined') {
-                router.push('/login')
-            }
-            return null
-        }
+    // If user is logged in, return original component
+    return <Component {...props} />
+  }
 
-        // If user is logged in, return original component
-        return (
-            <Component {...props} />
-        );
-    };
+  return Auth
+}
 
-    return Auth;
-};
-
-export default withAuth;
+export default withAuth
